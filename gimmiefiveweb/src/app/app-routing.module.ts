10b9878ed4fb224466f3e64db4a72e61ded27b88
Danielle Montecer <150003359+dnllmntcr@users.noreply.github.com>
2023-11-06 18:15:43 +0800

    first commit

--- gimmiefiveweb/src/app/app-routing.module.ts
+++ gimmiefiveweb/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { RegisterComponent } from './register/register.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'shop', component: ShopComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
